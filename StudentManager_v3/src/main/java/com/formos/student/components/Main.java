9d9fbb3b65320a442b8c9347a37fda4453e3a954
thong.giang <thong.giang@formos_vn_16>
2014-09-29 11:27:12 +0700

    Thong test6

--- StudentManager_v3/src/main/java/com/formos/student/components/Main.java
+++ StudentManager_v3/src/main/java/com/formos/student/components/Main.java
@@ -8,7 +8,7 @@ package com.formos.student.components;
  *
  */
 public class Main {
- //	comment here - 2131312312 555555 - 32132131
+ //	comment here - 2131312312 555555 - 32132131	//Thong test6
 //	comment 3
 //	comment 4
 //  Duy commits here
