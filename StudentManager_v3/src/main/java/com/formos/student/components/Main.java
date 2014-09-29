ad6b39ed45f5615f8cab389ad45611f5f690fc5b
thong.giang <thong.giang@formos_vn_16>
2014-09-29 10:18:39 +0700

    Thong test5

---
+++ StudentManager_v3/src/main/java/com/formos/student/components/Main.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.formos.student.components;
+
+/**
+ * @author duy.chung
+ *
+ */
+public class Main {
+//	comment here
+//	comment 2
+//  Duy commits here
+//	Thong test1
+} // comment more
+
+
+// Thong test2 test3
