a07a2532aeff1382faa26b6e20aeb1df28bafaeb
duymap <chungkhanhduy1985@gmail.com>
2014-10-10 21:51:00 +0700

    update

--- StudentManager_v3/src/main/java/com/formos/student/components/Layout.java
+++ StudentManager_v3/src/main/java/com/formos/student/components/Layout.java
@@ -4,18 +4,9 @@ package com.formos.student.components;
  * Layout component for pages of application StudentManager.
  */
 public class Layout {
-	/*
-	@Property
-	private String pageName;
-
-	@Inject
-	private ComponentResources resources;
-
-	public String getClassForPageName() {
-		return resources.getPageName().equalsIgnoreCase(pageName) ? "current_page_item"
-				: null;
-	}
-	*/
+	 public Layout() {
+		 
+	 }
 }
  //Thong test5
 
--- StudentManager_v3/src/main/java/com/formos/student/components/Main.java
+++ StudentManager_v3/src/main/java/com/formos/student/components/Main.java
@@ -8,20 +8,9 @@ package com.formos.student.components;
  *
  */
 public class Main {
- //	comment here - 2131312312 555555 - LLLLLLL //Thong test6
-//	comment 3
-//	comment 4
-//  Duy commits here
-//	Thong test1
+	public Main() {
+		
+	}
 } // comment more
 
 
-// Thong test2 test3
-
-//Thong test new test_merge
-
-//Duy modfidied
-=======
-//Duy modfidied
-
-//Duy modfidied 2
