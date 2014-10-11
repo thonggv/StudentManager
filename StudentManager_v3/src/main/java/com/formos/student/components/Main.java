f6a3d60dfd60d4abb9502cf3088a5761e94d2760
duymap <chungkhanhduy1985@gmail.com>
2014-10-11 07:58:15 +0700

    push demo

--- StudentManager_v3/src/main/java/com/formos/student/components/Main.java
+++ StudentManager_v3/src/main/java/com/formos/student/components/Main.java
@@ -2,20 +2,12 @@
  * 
  */
 package com.formos.student.components;
-
 /**
  * @author duy.chung
  *
  */
 public class Main {
 
-	// sua luon dasd dasdsa dsadas
-	// sua luon
-	public Main() { dsadas
-		// sua
-		// sua file Main
-	}
-	// sua
-} // comment more
+} 
 
 
